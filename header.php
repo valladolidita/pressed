6f5546981a4c251f9e59c03bdddef1575bc3b73a
valladolidita <kelly@Kellys-MacBook-Pro.local>
2015-02-12 15:46:12 -0500

    more dynamic changes to header

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php bloginfo('name'); ?></title>
-	<script src="http://use.edgefonts.net/josefin-slab.js"></script>
-	<script src="http://use.edgefonts.net/merriweather.js"></script>
 	<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>">
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"/>
 	<?php wp_head(); ?>
+	<title><?php bloginfo('name'); ?></title>
+	<script src="http://use.edgefonts.net/josefin-slab.js"></script>
+	<script src="http://use.edgefonts.net/merriweather.js"></script>
 </head>
 	<body <?php body_class( $class ); ?>>
 		<header>
